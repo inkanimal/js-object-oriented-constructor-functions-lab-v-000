7e261cbd4e3c373dd00936689ea1ae7eeb3064bb
geoffrey woodcock <theatomproject@gmail.com>
2019-05-19 16:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function Scooter(year, color, model) {
   this.color = color
   this.model = model
 }
+
+function Scooter(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function Scooter(address, city) {
+  this.address = address
+  this.city = city
+  
+}
